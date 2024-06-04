26e11a6c5752478b371c18050010b8394f6cd577
mathieu amacher <amacher_mathieu@hotmail.com>
2024-06-04 18:24:39 +0200

    preproc img

--- depth_planes/logic/preprocessor.py
+++ depth_planes/logic/preprocessor.py
@@ -1,6 +1,14 @@
 # Preprocessing
-from depth_planes.params import *
+# from depth_planes.params import *
 import tensorflow as tf
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import io, image
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
 def preprocess_bulk():
     pass
@@ -38,26 +46,52 @@ def preprocess_one_image(paths_list: list, type: str) -> np.ndarray:
 
         save_data(file,'preprocessed')
 
+
+
+# preproc du y
 def preprocess_exr_to_array(path: str) -> np.ndarray:
     """
-    _summary_
+    l'image(y) est chargé depuis son path
     """
-    pass
+    # print('le path***********************************************', path)
 
-def preprocess_jpg_to_array(path: str) -> np.ndarray:
-    """
-    _summary_
-    """
-    pass
+    img = cv2.imread(path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
-def preprocess_png_to_array(path: str) -> np.ndarray:
+    im=img*65535
+    im[im>65535]=65535
+    png_img = im.astype('uint16')
+
+    img_y = cv2.resize(png_img, dsize=(256, 128), interpolation=cv2.INTER_CUBIC)
+
+    # print('*****************', img_y)
+
+    return img_y
+
+
+#preprocess le X
+def preprocess_img_to_array(path: str) -> np.ndarray:
     """
     _summary_
     """
-    pass
+    # print('**************************', path)
+
+    img = load_img(path)
+    load_image_to_array = img_to_array(img)
+    img_standardization = tf.image.per_image_standardization(load_image_to_array)
+    img_x = tf.image.resize(img_standardization, [128, 512], preserve_aspect_ratio=True)
+
+    # print('**********************', img_x)
+    return img_x
+
 
 def preprocess_mat_to_array(path: str) -> np.ndarray:
     """
     _summary_
     """
     pass
+
+
+if __name__ == '__main__':
+    # preprocess_img_to_array('../../raw_data/rgb/rgb_0001.png')
+    # preprocess_mat_to_array()
+    # preprocess_exr_to_array('../../raw_data/depth/depth_0005.exr')
