e1123d48d1911160bca9eb3ac6399ff3ed3fef8e
Jean-baptiste Olivier <jean-baptiste@olivier.contact>
2024-06-04 23:05:31 +0200

    Update : data.py - save : ok - load : ok

--- depth_planes/logic/data.py
+++ depth_planes/logic/data.py
@@ -1,51 +1,84 @@
 # GCP logic implementation
 from depth_planes.params import *
 import os
+import numpy as np
+import datetime
 from google.cloud import storage
-
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='/home/jbo/code/soapoperator/gcp/neon-fiber-420411-dd5c2fc94b59.json'
 
 def get_data(path: str) -> list:
     """
     _summary_
     """
 
+    images = []
+
     if IMAGE_ENV == 'local':
         folder = "{}/{}".format(os.path.dirname(os.path.dirname(os.getcwd())),path)
-        images = []
         for image in os.listdir(folder):
             images.append(folder + image)
 
     if IMAGE_ENV == 'gcp':
+        print('test')
         client = storage.Client(project=GCP_PROJECT)
+        print(BUCKET_NAME)
         for blob in client.list_blobs(BUCKET_NAME, prefix=path):
-            print(str(blob))
-            # blob.download_to_filename(destination_file_name)
+            images.append(blob.name)
+            #get_blob_url(blob)
 
     print(images)
     return images
 
-def save_data(file_array,
-    name:str,
-    path: str):
+def save_data(file_array: str, name:str, path: str):
     """
-    - Load data from the bucket and a specific bloob
-    - ???
+    Save np.array to a npy file in a bucket and a specific folder
     """
 
     if IMAGE_ENV == 'local':
-        with open(f'{path}/{name}.npy', 'wb') as f:
-            np.save(f,file)
+        # Check if the folder exists
+        folder = "{}/{}".format(os.path.dirname(os.path.dirname(os.getcwd())),path)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        # Create and save the npy file
+        file_path = f'{folder}/{name}.npy'
+        np.save(file_path, file_array)
 
     if IMAGE_ENV == 'gcp':
 
-        # Iterate on the bucket
+        # Init GCP storage
         client = storage.Client(project=GCP_PROJECT)
         bucket = client.bucket(BUCKET_NAME)
 
-        destination_uri = '{}/{}'.format(bucket_folder,blob.name)
-        blob.download_to_filename(destination_uri)
+        # Convert the file_array to bytes
+        file_bytes = file_array.encode('utf-8')
+
+        # Create a unique blob name
+        blob_name = f"{path}/{name}.npy"
+
+        # Upload the file to the bucket
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(file_bytes, content_type='application/octet-stream')
+
+def get_blob_url(blob_prefix: str) -> str:
+
+    client = storage.Client(project=GCP_PROJECT)
+    bucket = client.get_bucket(BUCKET_NAME)
+    blob = bucket.get_blob(blob_prefix)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=30),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    print("Generated GET signed URL:")
+    print(url)
+
+    return url
 
 if __name__ == '__main__':
-    get_data('raw_data')
-    #save_data()
+    #get_data('urbansyn/rgb')
+    #save_data('[[1,1][1,1]]','test','doss/perferct')
+    #get_blob_url('urbansyn/rgb/rgb_7539.png')
--- depth_planes/logic/preprocessor.py
+++ depth_planes/logic/preprocessor.py
@@ -1,25 +1,42 @@
 # Preprocessing
 # from depth_planes.params import *
-import tensorflow as tf
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow import io, image
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import scipy.io
+import h5py
 
-os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
 def preprocess_bulk():
     pass
 
-    # img_list_normalized = []
-    # for i in range(len(img_list)):
-    #     img_normalize = img_list[i] / 255
-    #     img_list_normalized.append(img_normalize)
-    #     X = np.array(img_list_normalized)
+    if DATA_URBANSYN:
+        file_list = get_data()
 
-def preprocess_one_image(paths_list: list, type: str) -> np.ndarray:
+    if DATA_MAKE3D:
+        pass
+
+    if DATA_DIODE:
+        pass
+
+    if DATA_MEGADEPTH:
+        pass
+
+    if DATA_DIMLRGBD:
+        pass
+
+    if DATA_NYUDEPTHV2:
+        pass
+
+    save_data(file_array: str, name:str, path: str)
+
+
+def preprocess_one_image(path: str, type: str) -> np.ndarray:
     """
     _summary_
 
@@ -30,22 +47,19 @@ def preprocess_one_image(paths_list: list, type: str) -> np.ndarray:
         np.ndarray: _description_
     """
 
-    for path in paths_list:
+    if type == 'img':
+        file_array = preprocess_img_to_array(path)
 
-        if type == 'jpg':
-            pass
+    if type == 'exr':
+        file_array = preprocess_exr_to_array(path, log_scale_near=10, log_scale_far=1, log_scale_medium=5)
 
-        if type == 'png':
-            pass
+    if type == 'mat':
+        pass
 
-        if type == 'exr':
-            pass
+    if type == 'h5':
+        pass
 
-        if type == 'mat':
-            pass
-
-        save_data(file,'preprocessed')
-
+    return file_array
 
 
 # preproc du y
@@ -69,6 +83,7 @@ def preprocess_exr_to_array(path, log_scale_near=10, log_scale_far=1, log_scale_
     png_img = img_log_combined_scaled.astype('uint16')
 
     res = cv2.resize(png_img, dsize=(512, 256), interpolation=cv2.INTER_CUBIC)
+
     return res
 
 
@@ -102,10 +117,16 @@ def preprocess_mat_to_array(path: str) -> np.ndarray:
     """
     _summary_
     """
-    pass
+    mat = scipy.io.loadmat(mat_path)
+    return mat
 
+def preprocess_h5_to_array(path: str) -> np.ndarray:
+    """
+    _summary_
+    """
+    hf = h5py.File(path, 'r')
 
 if __name__ == '__main__':
     # preprocess_img_to_array('../../raw_data/rgb/rgb_0001.png')
-    # preprocess_mat_to_array()
+    preprocess_mat_to_array('/home/jbo/code/soapoperator/depth-planes-from-2d/raw_data/make3d/depth/make3d_train_depth_depth_sph_corr-060705-17.10.14-p-018t000.mat')
     # preprocess_exr_to_array('../../raw_data/depth/depth_0005.exr')
