5ed12b9ae2d829c3576763d4861b0e0f3c87e073
Leslie Rolland <rolland.leslie@gmail.com>
2024-06-05 18:00:34 +0200

    added wrong params

---
+++ depth_planes/params.py
@@ -0,0 +1,28 @@
+import os
+
+##################  VARIABLES  ##################
+
+# DATASET
+IMAGE_ENV=os.environ.get("IMAGE_ENV")
+DATA_URBAN=os.environ.get("DATA_URBAN")
+DATA_MAKE3D=os.environ.get("DATA_MAKE3")
+DATA_DIODE=os.environ.get("DATA_DIODE")
+DATA_MEGADEPTH=os.environ.get("DATA_MEGADEPTH")
+DATA_DIMLRGBD=os.environ.get("DATA_DIMLRGBD")
+DATA_NYUDEPTHV2=os.environ.get("DATA_NYUDEPTHV2")
+IMAGE_SHAPE=os.environ.get("IMAGE_SHAPE")
+
+# MODEL
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'),
+                                    ".lewagon", "project",
+                                    "training_outputs")
+
+
+##################  CLOUD STORAGE  ##############
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
+BUCKET_NAME=os.environ.get("BUCKET_NAME")
+
+# Local
+LOCAL_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "raw_data")
