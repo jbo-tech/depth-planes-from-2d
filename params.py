b21ba74a018e054c78c1c914c39456269bc1bd4b
Jean-baptiste Olivier <jean-baptiste@olivier.contact>
2024-06-05 18:17:53 +0200

    Update : params.py

--- params.py
+++ params.py
@@ -2,23 +2,28 @@ import os
 
 ##################  VARIABLES  ##################
 
-# DATASET
+################## Configuration ##################
 IMAGE_ENV=os.environ.get("IMAGE_ENV")
-DATASET_URBAN=os.environ.get("DATASET_URBAN")
-DATASET_MAKE3D=os.environ.get("DATASET_MAKE3")
+DATA_URBANSYN=os.environ.get("DATA_URBANSYN")
+DATA_MAKE3D=os.environ.get("DATA_MAKE3")
+DATA_DIODE=os.environ.get("DATA_DIODE")
+DATA_MEGADEPTH=os.environ.get("DATA_MEGADEPTH")
+DATA_DIMLRGBD=os.environ.get("DATA_DIMLRGBD")
+DATA_NYUDEPTHV2=os.environ.get("DATA_NYUDEPTHV2")
 IMAGE_SHAPE=os.environ.get("IMAGE_SHAPE")
 
-# MODEL
-MODEL_TARGET=os.environ.get("MODEL_TARGET")
+################## Local ##################
+LOCAL_DATA_PATH=os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "raw_data")
 LOCAL_REGISTRY_PATH=os.path.join(os.path.expanduser('~'),
                                     ".lewagon", "project",
                                     "training_outputs")
-
-
-##################  CLOUD STORAGE  ##############
+##################  VARIABLES  ##################
 GCP_PROJECT=os.environ.get("GCP_PROJECT")
 GCP_REGION=os.environ.get("GCP_REGION")
 BUCKET_NAME=os.environ.get("BUCKET_NAME")
 
+################## Cloud Storage ##################
+BUCKET_NAME=os.environ.get("BUCKET_NAME")
 
-##################  COMPUTE ENGINE  #############
+################## Model ##################
+MODEL_TARGET=os.environ.get("MODEL_TARGET")
