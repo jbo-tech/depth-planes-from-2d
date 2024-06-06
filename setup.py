8591b61789e5b105b37c7b04fa143f67998d929e
LeslieRolland <rolland.leslie@gmail.com>
2024-06-06 15:33:53 +0000

    changes for VM to run

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='depth-planes',
-      version="0.0.1",
       install_requires=requirements,
       packages=find_packages(),
-      test_suite="tests",
       include_package_data=True,
       zip_safe=False)
