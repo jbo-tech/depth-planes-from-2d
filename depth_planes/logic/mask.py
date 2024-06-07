3ddf7224afadce0f9f52789a5d28ccb38b30d458
anthony pouillart <anthony.pouillart@gmail.com>
2024-06-07 10:52:56 +0200

    Create new file mask.py with skelet of 3 functions

---
+++ depth_planes/logic/mask.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def number_mask(y_pred) -> int:
+    """
+    Function to determine the number of mask to create from an image y_pred
+    """
+
+    pass
+
+def mask(y_pred, nb_mask: int =5) -> np.array:
+    """
+    Function to create nb_mask new images from an image predicted y_pred
+    """
+
+    pass
+
+def apply_mask(x, mask:np.array) -> np.array:
+    """
+    Function to apply mask on the reel image X
+    """
+
+    pass
