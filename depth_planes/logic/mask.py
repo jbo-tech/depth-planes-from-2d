aed6bc59411a1deb1ba1f6e2d5e6e0b09d3b9cfc
anthony pouillart <anthony.pouillart@gmail.com>
2024-06-12 17:45:07 +0200

    Add docstring to create_mask_matricial function

--- depth_planes/logic/mask.py
+++ depth_planes/logic/mask.py
@@ -42,6 +42,12 @@ def create_mask_in_one(y_pred, nb_mask: int =None) -> np.array:
     return mask
 
 def create_mask_matricial(y_pred):
+    """
+    Function to create an array representing categorical mask
+        with matricial calculus
+    Input:
+    y_pred.shape -> (128,256,1)
+    """
     return np.round(y_pred/10000)
 
 def apply_mask(x, mask:np.array) -> np.array:
